6b2a05acf775aee4ab4ffea060a0e674f8d2941e
sai-sreekhar <sai.sreekhar@gmail.com>
2023-10-31 11:57:06 +0530

    feat: edited profile

--- task-6/src/components/Projects.js
+++ task-6/src/components/Projects.js
@@ -1,29 +1,61 @@
-import React from 'react';
+import React from "react";
 
 const Projects = () => {
   return (
     <section id="projects">
       <h2>Experience</h2>
-      <p><strong>Samsung R&D Institute</strong> | December 2022 - August 2023</p>
-      <p><strong>Prism Developer</strong> | Remote</p>
+      <p>
+        <strong>Samsung R&D Institute</strong> | December 2022 - August 2023
+      </p>
+      <p>
+        <strong>Prism Developer</strong> | Remote
+      </p>
       <ul>
-        <li>Developed a Bixby feature with Natural Language Processing (NLP) for users to instruct Bixby to store information and set task reminders.</li>
-        <li>Enhanced Bixby’s functionality, allowing it to retrieve and provide stored information upon user requests.</li>
-        <li>Utilized the Bert model for accurate relation extraction and Neo4j for efficient data storage, facilitating the creation of a knowledge graph.</li>
+        <li>
+          Developed a Bixby feature with Natural Language Processing (NLP) for
+          users to instruct Bixby to store information and set task reminders.
+        </li>
+        <li>
+          Enhanced Bixby’s functionality, allowing it to retrieve and provide
+          stored information upon user requests.
+        </li>
+        <li>
+          Utilized the Bert model for accurate relation extraction and Neo4j for
+          efficient data storage, facilitating the creation of a knowledge
+          graph.
+        </li>
       </ul>
 
       <h2>Projects</h2>
-      <p><strong>FuturePreneurs Project, E-Cell VIT</strong> | August 2022 - October 2022</p>
-      <p><strong>GitHub Repository</strong> | [https://github.com/sai-sreekhar/ESummit-Backend23]</p>
+      <p>
+        <strong>FuturePreneurs Project, E-Cell VIT</strong> | August 2022 -
+        October 2022
+      </p>
+      <p>
+        <strong>GitHub Repository:</strong> |{" "}
+        <a href="https://github.com/sai-sreekhar/ESummit-Backend23">Link</a>
+      </p>
       <ul>
-        <li>Tools & Technologies used: NodeJS, ExpressJS, MongoDB, AWS, JMeter</li>
-        <li>Designed and developed the entire Backend module for FuturePreneurs project with NodeJS/ExpressJS and MongoDB.</li>
-        <li>This FuturePreneurs is deployed on Amazon EC2 instances. Amazon CloudFront is used for SSL termination and faster site loading. Route53 is used as DNS Server.</li>
-        <li>FuturePreneurs features include Authentication and Authorization module, Quiz module, and Team module.</li>
+        <li>
+          Tools & Technologies used: NodeJS, ExpressJS, MongoDB, AWS, JMeter
+        </li>
+        <li>
+          Designed and developed the entire Backend module for FuturePreneurs
+          project with NodeJS/ExpressJS and MongoDB.
+        </li>
+        <li>
+          This FuturePreneurs is deployed on Amazon EC2 instances. Amazon
+          CloudFront is used for SSL termination and faster site loading.
+          Route53 is used as DNS Server.
+        </li>
+        <li>
+          FuturePreneurs features include Authentication and Authorization
+          module, Quiz module, and Team module.
+        </li>
         <li>Performance testing is done using JMeter tool.</li>
       </ul>
     </section>
   );
-}
+};
 
 export default Projects;
